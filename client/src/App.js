3bd5a689dc042211b7fdd57c4315d0056cf5a1af
Emilio <ediaz-goico@me.bergen.edu>
2020-04-21 14:44:16 -0400

    finished mvp, github was down so this one was a little late

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import axios from 'axios'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
 
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+
+    getMovies();
+  }, []);
+
+  
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
   return (
+    
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route path="/movies/:id">
+            <Movie addToSavedList={addToSavedList} />
+          </Route>
+          <Route path="/">
+            <MovieList movies={movies} />
+          </Route>
+        </Switch>
+        </div>
     </div>
   );
 };
